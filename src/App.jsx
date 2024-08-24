f969477859fe62ad04b40f8638804de7595a356a
TravisLau92 <travislau92@hotmail.com>
2024-08-24 15:18:50 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import { AuthContext } from './pages/AuthContext';
+import RequireAuth from './pages/RequireAuth';
+import useLocalStorage from 'use-local-storage';
+
+export default function App() {
+  const [token, setToken] = useLocalStorage('token', null);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Login />} path="/login" />
+          <Route
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+            path="/dashboard"
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+}
